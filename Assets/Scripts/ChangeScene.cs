5747c56b44f58b23b11d9140122f4ddad2a161a0
KANGJUNHEE\강준희 <khs0348@gmail.com>
2018-06-17 00:22:26 +0900

    시작화면 이미지 추가 및 스테이지 선택 버튼 클릭시 Scene 전환 기능

---
+++ Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ChangeStageScene()
+    {
+        SceneManager.LoadScene("StageScene");
+    }
+    public void ChangeStage1()
+    {
+        SceneManager.LoadScene("Stage1");
+    }
+    public void ChangeStage2()
+    {
+        SceneManager.LoadScene("Stage2");
+    }
+    public void ChangeStage3()
+    {
+        SceneManager.LoadScene("Stage3");
+    }
+    public void ChangeStage4()
+    {
+        SceneManager.LoadScene("Stage4");
+    }
+    public void ChangeStage5()
+    {
+        SceneManager.LoadScene("Stage5");
+    }
+}
