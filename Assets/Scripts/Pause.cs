f852efcac7623472b3ccd463bceedbfff91993e8
KANGJUNHEE\강준희 <khs0348@gmail.com>
2018-06-17 18:02:07 +0900

    메인 이미지 변경 및 버튼 UI 추가,변경 및 인게임 멈춤,시작,홈버튼 생성

---
+++ Assets/Scripts/Pause.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class Pause : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+    public GameObject stop;
+    public GameObject play;
+    public GameObject settingCanvas;
+
+
+    // Update is called once per frame
+    void Update () {
+
+        
+	}
+    public void Stop()
+    {
+        Time.timeScale = 0;//일시정지
+        stop.SetActive(true);
+
+        if (settingCanvas) settingCanvas.gameObject.SetActive(true);
+    
+    }
+
+    public void Play()
+    {
+        Time.timeScale = 1;//다시시작
+        play.SetActive(true);
+        if (settingCanvas) settingCanvas.gameObject.SetActive(false);
+    }
+
+public void Quit()
+    {
+        Application.Quit();
+    }
+}
